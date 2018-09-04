6c0f8eb4c8dcebdbbacf186c905446d99d4e0466
balaji2411 <42370720+balaji2411@users.noreply.github.com>
2018-09-04 10:28:43 +0530

    edited

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Balaji S";
+    private final String message = "Hello Balaji Shanmugam";
 
     public App() {}
 
